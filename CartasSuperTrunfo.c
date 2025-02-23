ef1aff697f537a6b03b3f274256c9d76b7c0483e
Lukas-css <lcordeirosan@gmail.com>
2025-02-23 15:54:00 +0000

    criei as CartasSuperTrunfo

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -18,5 +18,83 @@ int main() {
     // Sugestão: Utilize a função printf para exibir as informações das cartas cadastradas de forma clara e organizada.
     // Exiba os valores inseridos para cada atributo da cidade, um por linha.
 
+   char estado;
+   char codigo[4];
+   char cidade[50];
+   int populacao;
+   float area;
+   float pib;
+   int pontos_turisticos;
+   
+   printf("Carta 1 \n");
+
+   printf("Digite seu estado:");
+   scanf("%c", &estado);
+
+   printf("Digite seu código:");
+   scanf("%s", &codigo);
+
+   printf("Nome da cidade:");
+   scanf("%s", &cidade);
+
+   printf("População:");
+   scanf("%d", &populacao);
+
+   printf("Área:");
+   scanf("%f", &area);
+
+   printf("PIB:");
+   scanf("%f", &pib);
+
+   printf("Número de pontos Turísticos:");
+   scanf("%d", &pontos_turisticos);
+   
+   printf("\n");
+   printf("Carta 1 \n");
+   printf("Estado: %c \n", estado);
+   printf("Código: %s \n", codigo);
+   printf("Cidade: %s \n", cidade);
+   printf("População: %d milhões \n", populacao);
+   printf("Área: %f km² \n", area);
+   printf("PIB: %f bilhões de reais\n", pib);
+   printf("Números de pontos Turísticos: %d \n", pontos_turisticos);
+   
+   printf("\n");
+
+   printf("Carta 2 \n");
+
+   printf("Digite seu estado:");
+   scanf("%c", &estado);
+
+   printf("Digite seu código:");
+   scanf("%s", &codigo);
+
+   printf("Nome da cidade:");
+   scanf("%s", &cidade);
+
+   printf("População:");
+   scanf("%d", &populacao);
+
+   printf("Área:");
+   scanf("%f", &area);
+
+   printf("PIB:");
+   scanf("%f", &pib);
+
+   printf("Número de pontos Turísticos:");
+   scanf("%d", &pontos_turisticos);
+   
+   printf("\n");
+   printf("Carta 2 \n");
+   printf("Estado: %c \n", estado);
+   printf("Código: %s \n", codigo);
+   printf("Cidade: %s \n", cidade);
+   printf("População: %d milhões \n", populacao);
+   printf("Área: %f km² \n", area);
+   printf("PIB: %f bilhões de reais\n", pib);
+   printf("Números de pontos Turísticos: %d \n", pontos_turisticos);
+   
+   
     return 0;
 }
+
